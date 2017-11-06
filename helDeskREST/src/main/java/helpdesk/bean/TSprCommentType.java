a9e7b7626222d05b53b5d5272603fa1e5e8ff439
vasil <andr_vasil@mail.ru>
2017-11-06 15:31:49 +0300

    JPA версия

---
+++ helDeskREST/src/main/java/helpdesk/bean/TSprCommentType.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpdesk.bean;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author vasil
+ */
+@Entity
+@Table(name = "t_spr_comment_type", catalog = "helpdesk", schema = "public")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "TSprCommentType.findAll", query = "SELECT t FROM TSprCommentType t")
+    , @NamedQuery(name = "TSprCommentType.findById", query = "SELECT t FROM TSprCommentType t WHERE t.id = :id")
+    , @NamedQuery(name = "TSprCommentType.findByFName", query = "SELECT t FROM TSprCommentType t WHERE t.fName = :fName")})
+public class TSprCommentType implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 250)
+    @Column(name = "f_name")
+    private String fName;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "fCommentTypeId")
+    private List<TIncidentComment> tIncidentCommentList;
+
+    public TSprCommentType() {
+    }
+
+    public TSprCommentType(Long id) {
+        this.id = id;
+    }
+
+    public TSprCommentType(Long id, String fName) {
+        this.id = id;
+        this.fName = fName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFName() {
+        return fName;
+    }
+
+    public void setFName(String fName) {
+        this.fName = fName;
+    }
+
+    @XmlTransient
+    public List<TIncidentComment> getTIncidentCommentList() {
+        return tIncidentCommentList;
+    }
+
+    public void setTIncidentCommentList(List<TIncidentComment> tIncidentCommentList) {
+        this.tIncidentCommentList = tIncidentCommentList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof TSprCommentType)) {
+            return false;
+        }
+        TSprCommentType other = (TSprCommentType) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "helpdesk.bean.TSprCommentType[ id=" + id + " ]";
+    }
+    
+}
--- helDeskREST/src/main/java/helpdesk/heldeskrest/restAPI.java
+++ helDeskREST/src/main/java/helpdesk/heldeskrest/restAPI.java
@@ -6,6 +6,8 @@
 package helpdesk.heldeskrest;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import org.jboss.logging.Logger;
@@ -19,11 +21,14 @@ import org.jboss.logging.Logger;
 public class restAPI {
 
     final Logger log = Logger.getLogger(getClass().getName());
+    
+    @PersistenceContext
+    EntityManager em;
 
     @GET
     @Path("/test")
     public String test() {
-        log.info("test");
+        log.info("test => " + em);
 
         return "TEST";
     }
