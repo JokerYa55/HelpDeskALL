2223bce4f5c3e2e9486371b58a9c39218ea3ef6c
vasil <andr_vasil@mail.ru>
2017-11-06 15:10:44 +0300

    JPA версия

--- helDeskREST/src/main/java/helpdesk/heldeskrest/restAPI.java
+++ helDeskREST/src/main/java/helpdesk/heldeskrest/restAPI.java
@@ -8,20 +8,24 @@ package helpdesk.heldeskrest;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import org.jboss.logging.Logger;
 
 /**
  *
  * @author vasil
  */
-
 @Stateless
 @Path("/")
 public class restAPI {
-    
+
+    final Logger log = Logger.getLogger(getClass().getName());
+
     @GET
     @Path("/test")
-    public String test(){
+    public String test() {
+        log.info("test");
+
         return "TEST";
     }
-    
+
 }
