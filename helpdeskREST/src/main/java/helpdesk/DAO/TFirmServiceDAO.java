8eb9bd234492d76c9c31f25a1ae55d34fa719f79
vasil <andr_vasil@mail.ru>
2017-11-09 20:00:01 +0300

    JPA версия

---
+++ helpdeskREST/src/main/java/helpdesk/DAO/TFirmServiceDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helpdesk.DAO;
+
+import helpdesk.bean.TFirmService;
+
+/**
+ *
+ * @author vasil
+ */
+public class TFirmServiceDAO extends abstractClassDAO<TFirmService, Long>{
+    
+}
